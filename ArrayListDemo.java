9eb6277d9339ee2d1a4de674b280a82ded3627f1
Raveendhar <raveendhar2007@gmail.com>
2026-01-19 20:58:14 +0530

    Added ArrayList

---
+++ ArrayListDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        System.out.println("ArrayList: "+al);
+        
+    }
+}
