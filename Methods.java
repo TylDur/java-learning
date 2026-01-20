53c6a99155f21862ea53fff90f677442256cc401
Raveendhar <raveendhar2007@gmail.com>
2026-01-20 23:30:52 +0530

    Updated Methods

--- Methods.java
+++ Methods.java
@@ -1,29 +1,28 @@
 public class Methods {
 
-
-
  // using static because there is no need for an oject for calling the function
 
-   static void print(){    // without parameters without return type
+   // without parameters without return type
+   static void print(){    
        
     System.out.println("Hello World!");
     }
 
-    static int sum(int a , int b){        // with parameters with return type
+    // with parameters with return type
+    static int sum(int a , int b){        
        
         return a+b;
     }
 
-    static void product(int a , int b){   // with parameters without return type
+    // with parameters without return type
+    static void product(int a , int b){   
         
         System.out.println("The product: "+ (a*b)); 
     }
-
-
-
         public static void main(String[] args) {
+
         print();
-         System.out.println("The sum: "+(sum(3,4)));
+        System.out.println("The sum: "+(sum(3,4)));
         product(3,4 );
 
     }
